f4daaa5fb89c3c4ecb940bab2bb612bd371fb86c
RoushanFW196 <roushankumar6338@gmail.com>
2022-01-15 16:59:21 +0530

    adding data

--- src/Table.jsx
+++ src/Table.jsx
@@ -3,23 +3,14 @@ import React,{useState,useEffect} from 'react'
 import "./App.css"
 import Form from './Form';
 import Itemsincol from "./Tableitems";
-import {Table} from "antd";
 
     const Tablecontent=({data})=>{
 
-        const columns=[{
-            "profile":"",
-            "name":"roushan",
-            "Age":"",
-            "Salary":"",
-            "department":"",
-            "Address":"",
-            "maritalStatus":""
-        }]
+     
        return (
            
            <div className="user-table">
-      <Table dataSource={data} columns={columns} />;
+     
          <h1>hello</h1>
          <table>
          <thead>
@@ -44,7 +35,7 @@ import {Table} from "antd";
                             <td> <p className="data"> {el.username}</p></td>
                             <td><p className="data"> {el.Age}</p></td>
                             <td><p className="data">{el.Address}</p></td>
-                            <td><p className="data">{el.Department}</p></td>
+                            <td><p className="data">{el.department}</p></td>
                             <td><p className="data">{el.Salary}</p></td>
                             <td><p className="data">{el.maritalstatus==true ? <p>Yes</p>:<p>No</p> }</p></td>
                              <td> <img id="imkkk" src={el.file} alt="" /></td> 
